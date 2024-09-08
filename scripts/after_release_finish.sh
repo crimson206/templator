2a666b6083004a256de1c9e72d6ddb178b894aa8
Sisung Kim <sisung.kim1@gmail.com>
2024-09-08 12:50:56 +0200

    added input requesting line

--- scripts/after_release_finish.sh
+++ scripts/after_release_finish.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-release_tag=$1
+read -p "Enter the release tag: " release_tag
 
-# develop 브랜치로 전환합니다.
 git checkout develop
 
-# develop 브랜치를 원격 저장소에 push 합니다.
 git push origin develop
 
-# main 브랜치로 전환합니다.
 git checkout main
 
-# main 브랜치를 원격 저장소에 push 합니다.
 git push origin main
 
-# 새로 생성된 태그를 원격 저장소에 push 합니다.
 git push origin "$release_tag"
 
-echo "Release $release_tag has been finished and pushed to remote repository."
+echo "Release $release_tag has been finished and pushed to the remote repository."
